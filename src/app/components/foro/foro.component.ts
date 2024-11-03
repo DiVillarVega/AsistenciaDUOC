4c79907d83b546d7deb24536e778c696082933b1
juancho pedro <ange.diaz@duocuc.cl>
2024-11-03 16:57:36 -0300

    Foro avance

--- src/app/components/foro/foro.component.ts
+++ src/app/components/foro/foro.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Publicacion } from 'src/app/model/publicacion';
+import { Usuario } from 'src/app/model/usuario';
+import { APIClientService } from 'src/app/services/apiclient.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-foro',
@@ -8,8 +12,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForoComponent  implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {}
+  @ViewChild("topOfPage") topOfPage!: ElementRef;
+
+  usuario = new Usuario();
+  publicacion: Publicacion = new Publicacion();
+  publicaciones: any;
+
+
+  constructor(private authService: AuthService, private api:APIClientService) { }
+
+  ngOnInit() {
+    this.api.listaPublicaciones.subscribe((publicaciones) => {
+      publicaciones.reserve();
+    this.publicaciones = publicaciones;
+    });
+    this.authService.usuarioAutenticado.subscribe((usuario) => {
+      this.usuario = usuario? usuario : new Usuario();  
+    });
+  } // FALTA AVANZAR MIN 8:29 Video 13 Componente api para foro
 
 }
