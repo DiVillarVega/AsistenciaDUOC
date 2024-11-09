3a258c9f8409fe5c7a59fc4434e224d51e694aa7
Diego Villar Vega <caro.aguirre@duocuc.cl>
2024-11-08 21:37:18 -0300

    animación titulo

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 import { IonicModule } from '@ionic/angular';
@@ -7,6 +7,7 @@ import { NavController } from '@ionic/angular';
 import { arrowBackOutline, logOut, logOutOutline } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
 import { AuthService } from 'src/app/services/auth.service';
+import { AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-header',
@@ -20,14 +21,32 @@ import { AuthService } from 'src/app/services/auth.service';
     , TranslateModule // CGV-Permite usar pipe 'translate'
   ]
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit {
 
-  constructor(private navCtrl: NavController, private authService: AuthService) { 
+  @ViewChild('titulo',{ read: ElementRef }) itemTitulo!: ElementRef
+
+  constructor(private navCtrl: NavController, private authService: AuthService, private animationController: AnimationController) { 
     addIcons({ logOutOutline });
   }
 
   logout() {
     this.authService.logout();
   }
+
+  ngAfterViewInit(): void {
+    if (this.itemTitulo) {
+      this.animationController
+        .create()
+        .addElement(this.itemTitulo.nativeElement)
+        .iterations(Infinity)
+        .duration(6000)
+        .keyframes([
+          { offset: 0, transform: 'translateX(-100%)', opacity: 0.4 },  // Fuera de la pantalla por la izquierda
+          { offset: 0.5, transform: 'translateX(0)', opacity: 1 },      // Completamente visible en el centro
+          { offset: 1, transform: 'translateX(100%)', opacity: 0.4 }    // Fuera de la pantalla por la derecha
+        ])
+        .play();
+    }
+}
 
 }
